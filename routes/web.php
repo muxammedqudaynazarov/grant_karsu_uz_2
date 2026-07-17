d99eba2cde59e26ab48028cb26e3cdc422703f67
muxammedqudaynazarov <m.qudaynazarov@gmail.com>
2026-07-17 13:15:05 +0500

    F-4

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\QuizController;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\UserBookController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,6 +18,10 @@ Route::get('/', function () {
 Route::get('/login', [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 Route::get('/hemis', [HemisController::class, 'student'])->name('hemis');
+
+Route::get('/home/certificate/{uuid}/download', [UserBookController::class, 'download'])->name('certificate.download');
+Route::get('/home/certificate/check/{uuid}', [UserBookController::class, 'check'])->name('certificates.check');
+
 Route::prefix('home')->middleware(['auth'])->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::get('/lang/{locale}', [HomeController::class, 'lang'])->name('lang');
@@ -24,8 +29,6 @@ Route::prefix('home')->middleware(['auth'])->group(function () {
     //Route::resource('/tests', TestController::class)->only(['index']);
     Route::post('/tests/answer/upload', [TestController::class, 'uploadAnswer'])->name('tests.answer.upload');
     Route::resource('/userbooks', UserBookController::class)->only(['update']);
-    Route::get('/certificate/{uuid}/download', [UserBookController::class, 'download'])->name('certificate.download');
-    Route::get('/certificate/check/{uuid}', [UserBookController::class, 'check'])->name('certificates.check');
     Route::post('/tests/{test}/finish', [QuizController::class, 'finish'])->name('tests.finish');
     Route::resource('/hemis/enter/questions', QuestionController::class)->only(['create', 'store']);
     Route::resource('/hemis/show/students/results', DepartmentController::class)->only(['index']);
